45649ad1f023a771891b408ed5060c627cf2a393
Abdülkerim Sinan Car <sinancar5454@gmail.com>
2024-04-11 02:24:36 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+	public class ResultCategoryDto
+	{
+		public string CategoryID { get; set; }
+		public string CategoryName { get; set; }
+	}
+}
