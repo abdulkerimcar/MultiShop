137e50970ef520ce8da59b8e3452914588178e15
Abdülkerim Sinan Car <sinancar5454@gmail.com>
2024-06-30 16:41:06 +0300

    Basket Service Completed

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -28,6 +28,10 @@ namespace MultiShop.IdentityServer
 			{
 				Scopes = {"CargoFullPermission"}
 			},
+            new ApiResource("ResourceBasket")
+			{
+				Scopes = {"BasketFullPermission"}
+			},
 
 			new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
@@ -46,6 +50,7 @@ namespace MultiShop.IdentityServer
             new ApiScope("DiscountFullPermission","Full authority for discount operations"),
             new ApiScope("OrderFullPermission","Full authority for order operations"),
             new ApiScope("CargoFullPermission","Full authority for cargo operations"),
+            new ApiScope("BasketFullPermission","Full authority for basket operations"),
 			new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
 		};
 
@@ -66,7 +71,7 @@ namespace MultiShop.IdentityServer
             {
                 ClientId = "MultiShopManagerId",
                 ClientName = "MultiShop Manager User",
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                 ClientSecrets= {new Secret("multishopsecret".Sha256())},
                 AllowedScopes= { "CatalogReadPermission", "CatalogFullPermission" }
             },
@@ -76,9 +81,9 @@ namespace MultiShop.IdentityServer
             {
                 ClientId = "MultiShopAdminId",
                 ClientName = "MultiShop Admin User",
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                 ClientSecrets = {new Secret("multishopsecret".Sha256())},
-                AllowedScopes = { "CatalogFullPermission", "CatalogReadPermission", "DiscountFullPermission", "OrderFullPermission","CargoFullPermission",
+                AllowedScopes = { "CatalogFullPermission", "CatalogReadPermission", "DiscountFullPermission", "OrderFullPermission","CargoFullPermission","BasketFullPermission",
 					IdentityServerConstants.LocalApi.ScopeName,
                     IdentityServerConstants.StandardScopes.Email,
                     IdentityServerConstants.StandardScopes.OpenId,
---
+++ Services/Basket/MultiShop.Basket/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Basket.Dtos;
+
+namespace MultiShop.Basket.Services
+{
+	public interface IBasketService
+	{
+		Task<BasketTotalDto> GetBAsket(string userId);
+		Task SaveBAsket(BasketTotalDto basketTotalDto);
+		Task DeleteBAsket(string userId);
+
+	}
+}
